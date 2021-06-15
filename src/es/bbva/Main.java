b19d997f469349780c1b9468d7f2e31d7439f30a
Jordi Ascensión <jordiascension@enterpriseconsulting.es>
2021-06-15 14:34:07 +0200

    Initial commit with hello world implementation.

---
+++ src/es/bbva/Main.java
@@ -0,0 +1,11 @@
+package es.bbva;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        // shortcut and code snippet
+        System.out.println("Hello World");
+
+    }
+}
